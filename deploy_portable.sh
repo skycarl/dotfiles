cc19659882d967f48aed9cc2cbe378dddbbdfaaf
Skyler Carlson <skylercarlson28@gmail.com>
2025-01-17 21:06:47 -0800

    Update

--- deploy_portable.sh
+++ deploy_portable.sh
@@ -1,5 +1,19 @@
 #!/bin/zsh
+
 cp .tmux.conf ~
 cp .portable_config ~
 
-source ~/.zshrc
+if [[ -n "$ZSH_VERSION" ]]; then
+    RC_FILE=~/.zshrc
+elif [[ -n "$BASH_VERSION" ]]; then
+    RC_FILE=~/.bashrc
+else
+    RC_FILE=~/.profile
+fi
+
+SOURCE_LINE='if [ -f ~/.portable_config ]; then . ~/.portable_config; fi'
+if ! grep -Fxq "$SOURCE_LINE" "$RC_FILE"; then
+    echo "$SOURCE_LINE" >> "$RC_FILE"
+fi
+
+source "$RC_FILE"
