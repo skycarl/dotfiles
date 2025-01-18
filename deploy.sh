0b763b5e14127e3f28b58f7043fad50a7ad4f805
Skyler Carlson <skylercarlson28@gmail.com>
2025-01-17 21:35:49 -0800

    Use symlinks

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,31 @@
 #!/bin/zsh
 
-cp .zshrc ~
-cp .tmux.conf ~
-cp .bash_aliases ~
-cp .gitconfig ~
+create_symlink() {
+    local file=$1
+    local target=$2
+
+    if [ -e "$target" ]; then
+        echo "$target already exists"
+    else
+        ln -s "$file" "$target"
+        echo "Created symlink for $file"
+    fi
+}
+
+config_files=(
+    ".zshrc"
+    ".tmux.conf"
+    ".bash_aliases"
+    ".gitconfig"
+)
+
+# Create symlinks for config files
+for file in "${config_files[@]}"; do
+    create_symlink "$HOME/repos/dotfiles/$file" "$HOME/$file"
+done
 
 # 1Password CLI setup
 mkdir -p ~/.config/op
-cp plugins.sh ~/.config/op/plugins.sh
+create_symlink "$HOME/repos/dotfiles/plugins.sh" "$HOME/.config/op/plugins.sh"
 
 source ~/.zshrc
